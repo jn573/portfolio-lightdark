54105002749397589b6da8c76cfc763fcd0ae2ac
James <jn573@users.noreply.github.com>
2021-07-19 22:34:51 +0200

    Minor update

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const flicker = document.getElementById('flicker')
-let move = 5
 
-function moveSlider(e) {
-    if (move == 5) {
-        flicker.style.left = '40px'
-        move = 40
-    } else {
-        flicker.style.left = '5px'
-        move = 5
-    }
-}
+const slider = document.querySelector('.switch') //getElementById('flicker')
+
+slider.addEventListener('click',() => {
+    document.body.classList.toggle('dark')
+    document.getElementById('flicker').className="flicker-on" //classList.toggle('flicker-on')
+})
+
+
 
 
